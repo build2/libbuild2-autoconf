ed529d37e86a20c2178a410aab996187c6797392
Fred Helmesjö <fred.helmesjo@qualisys.se>
2022-08-30 16:01:13 +0200

    Add 'HAVE_SYS_SELECT_H'

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SYS_SELECT_H.h
@@ -0,0 +1,19 @@
+// HAVE_SYS_SELECT_H : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_SYS_SELECT_H
+
+/* Since OpenBSD 5.4, FreeBSD 5.0, NetBSD x, Mac OS 10.3 (assuming it was
+ * added roughly at the same time as OpenBSD 5.4), Linux Kernel 2.6.16
+ */
+#if defined(__linux__)                     || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(201311) || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 0)   || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)    || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0)    || \
+    BUILD2_AUTOCONF_GLIBC_PREREQ(2, 36)
+#  define HAVE_SYS_SELECT_H 1
+#endif
