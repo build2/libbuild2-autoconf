fbd841170687f5b231b3854279ad508cdb29becc
Fred Helmesjö <fred.helmesjo@qualisys.se>
2022-08-30 17:21:33 +0200

    HAVE_SYSCTL: Add __linux__

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SYSCTL.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SYSCTL.h
@@ -7,7 +7,8 @@
 #undef HAVE_SYSCTL
 /* Since 4.4BSD (~1995)
  */
-#if defined(__FreeBSD__) || \
+#if defined(__linux__)   || \
+    defined(__FreeBSD__) || \
     defined(__OpenBSD__) || \
     defined(__NetBSD__)  || \
     defined(BUILD2_AUTOCONF_MACOS)
