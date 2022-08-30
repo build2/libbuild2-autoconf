cfce2e888707aa52074f5f251560e5559e10eac7
Fred Helmesjö <fred.helmesjo@qualisys.se>
2022-08-30 13:00:03 +0200

    Add 'HAVE_FCNTL'

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FCNTL.h
@@ -0,0 +1,16 @@
+// HAVE_FCNTL : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_FCNTL
+/* Since BSD 4.2 (1983)
+ */
+#if defined(__linux__)   || \
+    defined(__FreeBSD__) || \
+    defined(__OpenBSD__) || \
+    defined(__NetBSD__)  || \
+    defined(BUILD2_AUTOCONF_MACOS)
+#  define HAVE_FCNTL 1
+#endif
