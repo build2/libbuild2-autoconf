2f6ddc3d3f394cd8a888f78a224896f8378dbe0b
Fred Helmesjö <fred.helmesjo@qualisys.se>
2022-08-30 14:59:08 +0200

    Add 'HAVE_SYSCTL'

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SYSCTL.h
@@ -0,0 +1,15 @@
+// HAVE_SYSCTL : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_SYSCTL
+/* Since BSD 4.4 (~1995)
+ */
+#if defined(__FreeBSD__) || \
+    defined(__OpenBSD__) || \
+    defined(__NetBSD__)  || \
+    defined(BUILD2_AUTOCONF_MACOS)
+#  define HAVE_SYSCTL 1
+#endif
