ea869b65a7af1f4bc8f649292c64ff396de2eedc
Fred Helmesjö <fred.helmesjo@qualisys.se>
2022-08-30 18:57:18 +0200

    Add 'HAVE_SENDFILE'

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SENDFILE.h
@@ -0,0 +1,15 @@
+// HAVE_SENDFILE : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_SENDFILE
+
+/* Since FreeBSD 3.0, Mac OS 10.5, glibc 2.1
+ */
+#if BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 0)   || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 5)    || \
+    BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)
+#  define HAVE_SENDFILE 1
+#endif
