390a3cea27ba9aa694c3512d4cd70551ff248814
Fred Helmesjö <fred.helmesjo@qualisys.se>
2022-08-30 11:04:33 +0200

    Fixed 'HAVE_DEVPOLL' typo.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_DEVPOLL.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_DEVPOLL.h
@@ -1,14 +1,14 @@
-// HAVE_EPOLL : BUILD2_AUTOCONF_LIBC_VERSION
+// HAVE_DEVPOLL : BUILD2_AUTOCONF_LIBC_VERSION
 
 #ifndef BUILD2_AUTOCONF_LIBC_VERSION
 #  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
 #endif
 
-#undef HAVE_EPOLL
+#undef HAVE_DEVPOLL
 
 /* Since Solaris version ... (?). No version details in manual.
  */
 #if (defined(__sun) && defined(__SVR4)) || \
     defined(__sun__)
-#  define HAVE_EPOLL 1
+#  define HAVE_DEVPOLL 1
 #endif
