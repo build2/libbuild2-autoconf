b4d7b3da53cf35a5ad9cac72b0c00c5b0254a320
Fred Helmesjö <fred.helmesjo@qualisys.se>
2022-08-30 18:14:21 +0200

    Add 'HAVE_NETINET_IN_H', 'HAVE_NETINET_IN6_H', 'HAVE_NETINET_TCP_H' (hard to find details, but most used by TCP & that is old (4.2BSD)

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_NETINET_IN6_H.h
@@ -0,0 +1,17 @@
+// HAVE_NETINET_IN6_H : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_NETINET_IN6_H
+
+/* No decent info...
+*/
+#if defined(__linux__)   || \
+    defined(__FreeBSD__) || \
+    defined(__OpenBSD__) || \
+    defined(__NetBSD__)  || \
+    defined(BUILD2_AUTOCONF_MACOS)
+#  define HAVE_NETINET_IN6_H 1
+#endif
