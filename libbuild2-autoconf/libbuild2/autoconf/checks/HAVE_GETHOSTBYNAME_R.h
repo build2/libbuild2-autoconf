a49a11edc8bb99de87dfc691e7420845af7b4335
EmilRosenquist <Emil.Rosenquist@gmail.com>
2022-08-31 12:04:48 +0200

    Add gethostbyname_r versions

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_GETHOSTBYNAME_R.h
@@ -0,0 +1,16 @@
+// HAVE_GETHOSTBYNAME_R : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_GETHOSTBYNAME_R
+
+/* Since Linux/glibc 2.1, SunOS, AIX and HPUX. 
+ */
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)       || \
+    defined(__SunOS) && defined(__SunOS_5_5) || \   
+    defined(_AIX) || \ 
+    defined(__hpux)  
+#  define HAVE_GETHOSTBYNAME_R 1
+#endif
---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_GETHOSTBYNAME_R_6_ARG.h
@@ -0,0 +1,13 @@
+// HAVE_GETHOSTBYNAME_R_6_ARG : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_GETHOSTBYNAME_R_6_ARG
+
+/* Since Linux/glibc 2.1
+ */
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)
+#  define HAVE_GETHOSTBYNAME_R_6_ARG 1
+#endif
