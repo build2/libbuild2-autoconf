cc3a53ddbc5d06a068dcb14e0450f9517a390736
EmilRosenquist <Emil.Rosenquist@gmail.com>
2022-08-31 15:28:41 +0200

    Add AFUNIX_H

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_AFUNIX_H.h
@@ -0,0 +1,11 @@
+// HAVE_AFUNIX_H
+#ifdef _MSC_VER
+#include <sdkddkver.h>
+#endif
+
+#if defined(__MINGW32__) && __MINGW32_MAJOR_VERSION >= 11 || \
+    defined(__MINGW64__) && __MINGW64_VERSION_MAJOR >= 11 || \
+    defined(_MSC_VER) && defined(WDK_NTDDI_VERSION) && \
+    WDK_NTDDI_VERSION >= NTDDI_WIN10_19H1
+#define HAVE_AFUNIX_H 1
+#endif
