5659225739758d86c63f6cea255a34b8e65f980b
Emil Rosenquist <37864286+EmilRosenquist@users.noreply.github.com>
2022-09-01 10:12:44 +0200

    Add HAVE___func__ and HAVE___FUNCTION__ (GH PR #27)

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE___FUNCTION__.h
@@ -0,0 +1,10 @@
+// HAVE___FUNCTION__
+
+#undef HAVE___FUNCTION__
+
+/* HAVE___FUNCTION__
+ * __FUNCTION__ is another name for __func__, provided for backward
+ * compatibility with old versions of GCC.
+ * Although it is seemingly available everywhere else.
+ */
+#define HAVE___FUNCTION__ 1
---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE___func__.h
@@ -0,0 +1,10 @@
+// HAVE___func__
+
+#undef HAVE___func__
+
+/* HAVE___func__
+ * __func__ was added to the standard in C99 and C++11. But seemingly exists
+ * almost everywhere before that as well.
+ */
+
+#define HAVE___func__ 1
