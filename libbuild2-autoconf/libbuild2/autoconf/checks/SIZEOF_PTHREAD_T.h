0364b16e8c3b245b50d50d74ec590af720c2610c
Emil Rosenquist <37864286+EmilRosenquist@users.noreply.github.com>
2022-09-01 09:14:59 +0200

    Add SIZEOF_{SHORT,INT,LONGLONG,PTHREAD_T} (GH PR #20)

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/SIZEOF_PTHREAD_T.h
@@ -0,0 +1,15 @@
+// SIZEOF_PTHREAD_T
+
+#undef SIZEOF_PTHREAD_T
+
+#ifndef _WIN32
+#   ifdef defined(__APPLE__)
+#       ifdef __LP64__
+#           define SIZEOF_PTHREAD_T 8
+#       else
+#           define SIZEOF_PTHREAD_T 4
+#       endif
+#   else
+#       define SIZEOF_PTHREAD_T 4
+#   endif
+#endif
