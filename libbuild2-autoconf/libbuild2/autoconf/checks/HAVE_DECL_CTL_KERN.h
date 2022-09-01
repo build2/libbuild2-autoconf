c504aa31a4a4e6f56b768f56be25fc69bff43741
Fred Helmesjö <helmesjo@gmail.com>
2022-09-01 10:07:44 +0200

    Add HAVE_DECL_CTL_KERN, HAVE_DECL_KERN_ARND (GH PR #38)

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_DECL_CTL_KERN.h
@@ -0,0 +1,18 @@
+// HAVE_DECL_CTL_KERN : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_DECL_CTL_KERN
+/* Since 4.4BSD (OpenBSD 2.1-6.2, FreeBSD, NetBSD, Mac OS X 10.7),
+ * <= glibc 2.31.
+ * NOTE: Appears to have been available parallel to sysctl()
+ */
+#if (BUILD2_AUTOCONF_OPENBSD_PREREQ(199706) && !BUILD2_AUTOCONF_OPENBSD_PREREQ(201804)) || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(2, 3)   || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)    || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 7)    || \
+    !BUILD2_AUTOCONF_GLIBC_PREREQ(2, 32)
+#  define HAVE_DECL_CTL_KERN 1
+#endif
