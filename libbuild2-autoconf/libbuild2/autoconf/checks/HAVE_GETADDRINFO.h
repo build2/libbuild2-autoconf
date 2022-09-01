905f98df1333192b315204bc263fc86943e8529e
Emil Rosenquist <37864286+EmilRosenquist@users.noreply.github.com>
2022-09-01 08:41:26 +0200

    Add GETADDRINFO, GETEGID, GETEUID, GETIFADDRS, GETPROTOBYNUMBER, GETSERVBYNAME, GETTIMEOFDAY (GH PR #4)

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_GETADDRINFO.h
@@ -0,0 +1,19 @@
+// HAVE_GETADDRINFO : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_GETADDRINFO
+
+/* Since Linux/glibc 2.4, OpenBSD 2.9, FreeBSD 3.5, NetBSD 1.5, and Mac OS
+ * (hard to tell but based on the manpages available online it looks like 
+ * Tiger at the latest).
+ */
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 4) || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 5)|| \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(200106) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 5) || \
+    defined(BUILD2_AUTOCONF_MACOS)
+#  define HAVE_GETADDRINFO 1
+#endif
---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_GETEUID.h
@@ -0,0 +1,17 @@
+// HAVE_GETEUID : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_GETEUID
+
+/* Since Linux/glibc 2.4, OpenBSD 2.0, FreeBSD 1.0, NetBSD 1.0, and Mac OS
+ */
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 4) || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(1, 0)|| \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199610) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 0) || \
+    defined(BUILD2_AUTOCONF_MACOS)
+#  define HAVE_GETEUID 1
+#endif
