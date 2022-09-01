9e33532b2a623d98fcdc6586c0541b85f1b9c929
Emil Rosenquist <37864286+EmilRosenquist@users.noreply.github.com>
2022-09-01 09:22:21 +0200

    Add HAVE_UMASK (GH PR #23)

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_UMASK.h
@@ -0,0 +1,17 @@
+// HAVE_UMASK : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_UMASK
+
+/*  Since glibc 2.1, FreeBSD 1.0, OpenBSD 2.0, NetBSD 1.0 and MacOS
+ */
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)    || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(1, 0)  || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199610) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 0) || \
+    defined(BUILD2_AUTOCONF_MACOS)
+#  define HAVE_UMASK 1
+#endif
