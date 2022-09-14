664f5a752b778f46267dfd49ccf8d15831d4115f
Francois Kritzinger <francois@codesynthesis.com>
2022-09-14 11:30:23 +0200

    Fix HAVE_STRTOK_R

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STRTOK_R.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STRTOK_R.h
@@ -6,13 +6,14 @@
 
 #undef HAVE_STRTOK_R
 
-/* Since 4.4BSD (OpenBSD, FreeBSD, NetBSD, Mac OS X),
- * glibc 2.0
+/* Since 4.4BSD (OpenBSD, FreeBSD, NetBSD, Mac OS X), glibc 2.0, and Mingw-w64
+ * 3.3.0.
  */
-#if defined(__FreeBSD__)            || \
-    defined(__OpenBSD__)            || \
-    defined(__NetBSD__)             || \
-    defined(BUILD2_AUTOCONF_MACOS)  || \
-    BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0)
+#if defined(__FreeBSD__)               || \
+    defined(__OpenBSD__)               || \
+    defined(__NetBSD__)                || \
+    defined(BUILD2_AUTOCONF_MACOS)     || \
+    BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0) || \
+    BUILD2_AUTOCONF_MINGW_PREREQ(3, 3)
 #  define HAVE_STRTOK_R 1
 #endif
