f1bb160df1b8dba66ea06c0107481b26847c7d03
Fred Helmesjö <helmesjo@gmail.com>
2022-09-20 15:15:24 +0200

    Add HAVE_SIGNAL_H (GH PR #55)

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SIGNAL_H.h
@@ -0,0 +1,19 @@
+// HAVE_SIGNAL_H : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_SIGNAL_H
+
+/* Since Version 3 AT&T UNIX (older than 1979).
+ * Linux, FreeBSD, OpenBSD, NetBSD, Mac OS X, Solaris
+ */
+#if defined(__linux__)                    || \
+    defined(__FreeBSD__)                  || \
+    defined(__OpenBSD__)                  || \
+    defined(__NetBSD__)                   || \
+    defined(BUILD2_AUTOCONF_MACOS)        || \
+    ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
+#  define HAVE_SIGNAL_H 1
+#endif
