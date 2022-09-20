7ca697a29f9b9e8a08e16b290cf6f11790873b61
Boris Kolpackov <boris@codesynthesis.com>
2022-09-20 15:47:17 +0200

    53

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STRERROR_R_CHAR_P.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STRERROR_R_CHAR_P.h
@@ -1,13 +1,11 @@
-// HAVE_STRERROR_R_CHAR_P : BUILD2_AUTOCONF_LIBC_VERSION
-
-#ifndef BUILD2_AUTOCONF_LIBC_VERSION
-#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
-#endif
+// HAVE_STRERROR_R_CHAR_P
 
 #undef HAVE_STRERROR_R_CHAR_P
 
-/* GNU specific
+/* GNU-specific.
  */
-#if defined(_GNU_SOURCE) || (_POSIX_C_SOURCE < 200112L)
-#  define HAVE_STRERROR_R_CHAR_P 1
+#ifdef __GLIBC__
+#  if defined(_GNU_SOURCE) || (_POSIX_C_SOURCE < 200112L)
+#    define HAVE_STRERROR_R_CHAR_P 1
+#  endif
 #endif
