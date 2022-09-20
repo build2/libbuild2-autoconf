b85eeaf59b8f2b662369d78fd504a1175f6c96df
Boris Kolpackov <boris@codesynthesis.com>
2022-09-20 15:34:10 +0200

    52

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_AI_ADDRCONFIG.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_AI_ADDRCONFIG.h
@@ -6,11 +6,12 @@
 
 #undef HAVE_AI_ADDRCONFIG
 
-/* Since Linux/glibc 2.3.4, OpenBSD >=5.6, FreeBSD >=6.3
+/* Since Linux/glibc 2.3.4, OpenBSD >=5.6, FreeBSD >=6.3, Mac OS, Windows.
  */
 #if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 4)      || \
     BUILD2_AUTOCONF_OPENBSD_PREREQ(201411)  || \
     BUILD2_AUTOCONF_FREEBSD_PREREQ(6, 3)    || \
+    defined(BUILD2_AUTOCONF_MACOS)          || \
     defined(_WIN32)
 #  define HAVE_AI_ADDRCONFIG 1
 #endif
