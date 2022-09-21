f0cfefb085e74aeaf2c8809d79212f876a55f296
Fred Helmesjö <helmesjo@gmail.com>
2022-09-21 11:54:55 +0200

    Define HAVE_SYS_POLL_H for glibc >= 2.0

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SYS_POLL_H.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_SYS_POLL_H.h
@@ -1,5 +1,14 @@
-// HAVE_SYS_POLL_H
+// HAVE_SYS_POLL_H : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
 
 #undef HAVE_SYS_POLL_H
 
-/* All use poll.h: glibc, FreeBSD, OpenBSD, NetBSD, Mac OS. */
+/* All use poll.h: FreeBSD, OpenBSD, NetBSD, Mac OS.
+ * Since glibc 2.0.
+ */
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0)
+#  define HAVE_SYS_POLL_H 1
+#endif
