7207d1b0905591c8a9fb374bb193165692bb541e
Francois Kritzinger <francois@codesynthesis.com>
2024-01-17 16:02:16 +0200

    Add HAVE_POSIX_SHM and HAVE_POSIX_SEM

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_POSIX_SEM.h
@@ -0,0 +1,25 @@
+// HAVE_POSIX_SEM : BUILD2_AUTOCONF_LIBC_VERSION
+
+/* If HAVE_POSIX_SEM is defined then all of the POSIX semaphore system calls
+ * such as sem_open(), sem_close(), sem_unlink(), etc. are available.
+ */
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_POSIX_SEM
+
+/* Available on all versions of macOS.
+ *
+ * Since OpenBSD 3.1 based on checking the online manpages by version.
+ *
+ * Since glibc 2.3.3 (when NPTL was added).
+ */
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 4)     || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 0)   || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(200206) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)    || \
+    defined(BUILD2_AUTOCONF_MACOS)
+#  define HAVE_POSIX_SEM 1
+#endif
