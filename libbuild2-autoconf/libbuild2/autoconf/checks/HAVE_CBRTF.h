8eaaf5c7dca3ca55a6a62d94931b0a5648cb996e
helmesjo <helmesjo@gmail.com>
2024-08-22 18:31:07 +0200

    Define HAVE_CBRT* for solaris.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_CBRTF.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_CBRTF.h
@@ -2,14 +2,16 @@
 
 #undef HAVE_CBRTF
 
-/* Since C99, POSIX.1-2001, 4.3BSD, OpenBSD 2.1, FreeBSD 2.0, glibc 2.19, Windows
+/* Since C99, POSIX.1-2001, 4.3BSD,
+ * OpenBSD 2.1, FreeBSD 2.0, glibc 2.19, Windows, Solaris
  */
 #if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 19) || \
     BUILD2_AUTOCONF_FREEBSD_PREREQ(2, 0) || \
     BUILD2_AUTOCONF_OPENBSD_PREREQ(199706) || \
     BUILD2_AUTOCONF_NETBSD_PREREQ(1, 3) || \
     defined(_WIN32) || \
-    defined(__MINGW32__)
+    defined(__MINGW32__) || \
+    ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_CBRTF 1
 #endif
 
