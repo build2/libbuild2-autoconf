2f7ad17a2f9851e257aadbaf0d7f98a51fdab9d7
helmesjo <helmesjo@gmail.com>
2024-08-22 19:49:50 +0200

    Fix typo in with OpenBSD year

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_COPYSIGNF.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_COPYSIGNF.h
@@ -6,12 +6,12 @@
 
 #undef HAVE_COPYSIGNF
 
-/*  Since glibc 2.19, BSD 4.3, FreeBSD 1.0, OpenBSD 2.0, NetBSD 1.0, MacOS, Mingw-w64 2.0
+/*  Since glibc 2.19, 4.3BSD, FreeBSD 1.0, OpenBSD 2.0, NetBSD 1.0, MacOS, Mingw-w64 2.0
  *  POSIX.1, SVr4
  */
 #if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 19)    || \
     BUILD2_AUTOCONF_FREEBSD_PREREQ(1, 0)  || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(199518) || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199510) || \
     BUILD2_AUTOCONF_NETBSD_PREREQ(1, 0) || \
     BUILD2_AUTOCONF_MINGW_PREREQ(1, 0) || \
     defined(_WIN32) || \
