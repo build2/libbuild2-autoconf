35f4af9e27a56bb61899011c77d226bddcf65887
helmesjo <helmesjo@gmail.com>
2024-08-22 21:40:57 +0200

    Add HAVE_ISINF|FINITE|NORMAL|NAN

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ISNORMAL.h
@@ -0,0 +1,21 @@
+// HAVE_ISNORMAL : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_ISNORMAL
+
+/* Since Linux/glibc 2.0, FreeBSD 5.1, OpenBSD 4.4, NetBSD 1.3, Mac OS, Solaris
+ */
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0)     || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(5, 1)   || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(200910) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 3)    || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 6)    || \
+    BUILD2_AUTOCONF_MINGW_PREREQ(1, 0)     || \
+    defined(BUILD2_AUTOCONF_MACOS)         || \
+    defined(_WIN32)                        || \
+    ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
+#  define HAVE_ISNORMAL 1
+#endif
