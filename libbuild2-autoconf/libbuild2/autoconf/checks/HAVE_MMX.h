cae282343a43c9788a89e8bc8d15dc6ca69ca33d
helmesjo <helmesjo@gmail.com>
2024-09-11 17:15:17 +0200

    Add HAVE_MMX

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_MMX.h
@@ -0,0 +1,12 @@
+// HAVE_MMX
+
+#undef HAVE_MMX
+
+/* Presence of MMX instructions, which are extensions to the original
+ * MMX instruction set, available on AMD processors starting from K6-2.
+ * Since glibc 2.0 (x86), FreeBSD (x86), OpenBSD (x86), NetBSD (x86),
+ * MacOS 10.04 (x86)
+ */
+#if defined(__MMX__)
+#  define HAVE_MMX 1
+#endif
