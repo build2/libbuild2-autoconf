a1a5be17fe40fa48bae0ee4142068360e713d51b
helmesjo <helmesjo@gmail.com>
2024-09-11 17:15:06 +0200

    Tweak HAVE_ATAN2 & HAVE_ATAN2F.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ATAN2F.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ATAN2F.h
@@ -2,15 +2,20 @@
 
 #undef HAVE_ATAN2F
 
-/* Since ANSI C89/ISO C99, OpenBSD 2.6
+/* Presence of the atan2f() function, which is a
+ * non-standard POSIX function used to set the name of a thread.
+ * Since glibc 2.0, FreeBSD 1.0, OpenBSD 1.0, NetBSD 1.0, MacOS 10.0
+ * Solaris 2.0, Windows, MinGW
  */
-#if (!defined(BUILD2_AUTOCONF_MACOS) && !defined(__NetBSD__) && !defined(__sun)) && \
-    BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0) || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(2, 1) || \
-    BUILD2_AUTOCONF_OPENBSD_PREREQ(199105) || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 10) || \
-    BUILD2_AUTOCONF_MINGW_PREREQ(6, 0) || \
-    (defined(__sun) && defined(__SVR4))
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 0)    || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 0)  || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199610) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 3) || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0) || \
+    defined(_WIN32) || \
+    defined(__MINGW32__) || \
+    defined(__CYGWIN__) || \
+    ((defined(__sun) && defined(__SVR4)) || defined(__sun__))
 #  define HAVE_ATAN2F 1
 #endif
 
