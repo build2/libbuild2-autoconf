bf6d96dd0fac0e4dbcedbd516ababd05df76736d
helmesjo <helmesjo@gmail.com>
2024-10-15 01:47:36 +0200

    HAVE_X86ASM: Also check MSVC defines.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_X86ASM.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_X86ASM.h
@@ -5,6 +5,8 @@
 /* Presence of x86 assembly support, ensuring that the system can
  * execute inline assembly or external assembly for x86 processors.
  */
-#if defined(__i386__)
+#if defined(__i386__) || \
+    defined(__x86_64__) || \
+    (defined(_M_IX86) || defined(_M_X64)) && (defined(_MSC_VER) || defined(__GNUC__) || defined(__clang__))
 #  define HAVE_X86ASM 1
 #endif
