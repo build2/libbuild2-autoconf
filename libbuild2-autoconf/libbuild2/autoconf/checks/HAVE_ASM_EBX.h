7fdf94fbdd096a0e74a928ee8596dcc19d2650ba
helmesjo <helmesjo@gmail.com>
2024-10-21 12:28:22 +0200

    HAVE_ASM_EBP & HAVE_ASM_EBX enabled on x86_64 as well.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ASM_EBX.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ASM_EBX.h
@@ -2,9 +2,11 @@
 
 #undef HAVE_ASM_EBX
 
-/* x86 excluding ARM, PowerPC, MIPS & RISC-V.
+/* x86 (available on x86_64 under RBX) excluding ARM, PowerPC, MIPS & RISC-V.
  */
-#if defined(__i386__)
+#if defined(__i386__) || \
+    defined(__x86_64__) || \
+    (defined(_M_IX86) || defined(_M_X64))
 #  if !defined(__arm__) && !defined(__powerpc__) && \
       !defined(__ppc__) && \
       !defined(__mips__) && \
