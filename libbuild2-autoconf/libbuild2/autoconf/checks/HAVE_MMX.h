503aaaf55990233ced4006b3874775f9ed696064
helmesjo <helmesjo@gmail.com>
2024-10-21 16:29:46 +0200

    HAVE_MMX & HAVE_MMXEXT are available with MSVC for x86 & x86_64.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_MMX.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_MMX.h
@@ -5,8 +5,9 @@
 /* Presence of MMX instructions, which are extensions to the original
  * MMX instruction set, available on AMD processors starting from K6-2.
  * Since glibc 2.0 (x86), FreeBSD (x86), OpenBSD (x86), NetBSD (x86),
- * MacOS 10.04 (x86)
+ * MacOS 10.04 (x86), MSVC
  */
-#if defined(__MMX__)
+#if defined(__MMX__) || \
+    defined(_M_IX86) || defined(_M_X64)
 #  define HAVE_MMX 1
 #endif
