5fa403abeda237d2cb5236bdf86906da6e478114
helmesjo <helmesjo@gmail.com>
2024-10-28 23:31:06 +0100

    HAVE_ASM_EBP & HAVE_ASM_EBX not available on Windows. HAVE_KCMVIDEOCODECTYPE_HVEC is for MacOS/iOS only.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ASM_EBP.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ASM_EBP.h
@@ -2,15 +2,16 @@
 
 #undef HAVE_ASM_EBP
 
-/* x86 (available on x86_64 under RBP) excluding ARM, PowerPC, MIPS & RISC-V.
+/* Check for EBP (Extended Base Pointer) support in assembly on x86/x86_64,
+ * excluding unsupported architectures (ARM, PowerPC, MIPS, RISC-V)
+ * and excluding Windows x64 with MSVC due to calling convention limitations.
  */
-#if defined(__i386__) || \
-    defined(__x86_64__) || \
-    (defined(_M_IX86) || defined(_M_X64))
-#  if !defined(__arm__) && !defined(__powerpc__) && \
-      !defined(__ppc__) && \
-      !defined(__mips__) && \
-      !defined(__riscv__) && !defined(__riscv)
-#    define HAVE_ASM_EBP 1
-#  endif
+#if (defined(__i386__) || defined(__x86_64__) || defined(_M_IX86) || defined(_M_X64)) && \
+    !(defined(_WIN32) && defined(_MSC_VER) && defined(_M_X64)) && \
+    !defined(__arm__) && !defined(__aarch64__) && \
+    !defined(__powerpc__) && !defined(__ppc__) && \
+    !defined(__mips__) && !defined(__riscv)
+    #define HAVE_ASM_EBP 1
+#else
+    #define HAVE_ASM_EBP 0
 #endif
--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_KCMVIDEOCODECTYPE_HEVC.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_KCMVIDEOCODECTYPE_HEVC.h
@@ -8,9 +8,8 @@
 
 /* HEVC (High-Efficiency Video Coding),
  * also known as H.265 codec, in the media framework.
- * Since MacOS 10.13/iOS 11, Windows 10
+ * Since MacOS 10.13/iOS 11
  */
-#if BUILD2_AUTOCONF_MACOS_PREREQ(10, 13) || \
-    (defined(_WIN32) && (_WIN32_WINNT >= _WIN32_WINNT_WIN10))
+#if BUILD2_AUTOCONF_MACOS_PREREQ(10, 13)
 #  define HAVE_KCMVIDEOCODECTYPE_HEVC 1
 #endif
