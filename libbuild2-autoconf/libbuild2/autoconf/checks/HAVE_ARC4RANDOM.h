a6b00ed2c006c471978378471356b689dc206cd6
helmesjo <helmesjo@gmail.com>
2024-11-09 16:47:08 +0100

    Revert "HAVE_ARC4RANDOM: Not available on linux (unless libbsd is installed)." It's required for at least libevent and appears to be a reasonable assumption that it exists.
    This reverts commit 621a069277a5a5f61e6613dea517e1092cd7451d.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ARC4RANDOM.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ARC4RANDOM.h
@@ -7,11 +7,12 @@
 #undef HAVE_ARC4RANDOM
 
 /* Since OpenBSD 2.1, FreeBSD 2.3, NetBSD 2.0, Mac OS X 10.7
- * and glibc 2.36 (only via libbsd).
+ * and glibc 2.36.
  */
 #if BUILD2_AUTOCONF_OPENBSD_PREREQ(199706) || \
     BUILD2_AUTOCONF_FREEBSD_PREREQ(2, 3)   || \
     BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)    || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 7)
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 7)    || \
+    BUILD2_AUTOCONF_GLIBC_PREREQ(2, 36)
 #  define HAVE_ARC4RANDOM 1
 #endif
