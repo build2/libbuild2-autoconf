bed8f4cbab0c52469304634eef9d75962f4797fe
helmesjo <helmesjo@gmail.com>
2024-11-25 12:19:00 +0100

    Add HAVE_FSEEKO.h

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FSEEKO.h
@@ -0,0 +1,17 @@
+// HAVE_FSEEKO : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_FSEEKO
+
+/* This checks for fseeko(), which is a large-file-aware
+ * alternative to fseek().
+ */
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1) || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 0) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0) || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0)
+#  define HAVE_FSEEKO 1
+#endif
