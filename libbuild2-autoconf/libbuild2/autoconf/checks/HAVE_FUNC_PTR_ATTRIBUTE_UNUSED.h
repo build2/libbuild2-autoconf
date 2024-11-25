8b534b3e7ae28210e46e61e5c03687d094cac0a8
helmesjo <helmesjo@gmail.com>
2024-11-25 12:19:05 +0100

    Add HAVE_FUNC_PTR_ATTRIBUTE_UNUSED.h

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_UNUSED.h
@@ -0,0 +1,10 @@
+// HAVE_FUNC_PTR_ATTRIBUTE_UNUSED
+
+#undef HAVE_FUNC_PTR_ATTRIBUTE_UNUSED
+
+/* Checks if the compiler supports the unused attribute for function
+ * pointers, which can suppress warnings for unused function pointers.
+ */
+#if defined(__GNUC__) || defined(__clang__)
+#  define HAVE_FUNC_PTR_PTR_ATTRIBUTE_UNUSED 1
+#endif
