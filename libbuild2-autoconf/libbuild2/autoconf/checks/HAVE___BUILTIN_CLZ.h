e62d8c58161fc6eeeee58f561a4b94bff20e5eae
helmesjo <helmesjo@gmail.com>
2024-11-25 14:39:47 +0100

    Add HAVE___BUILTIN_CLZ.h

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE___BUILTIN_CLZ.h
@@ -0,0 +1,17 @@
+// HAVE___BUILTIN_CLZ
+
+#undef HAVE___BUILTIN_CLZ
+
+/* Check determines if the __builtin_clz()
+ * intrinsic is available. The function
+ * calculates the number of leading zeros
+ * in the binary representation of an integer.
+ * It is provided by GCC and Clang for various
+ * architectures and is highly optimized for
+ * hardware that supports efficient bit manipulation.
+ */
+#if ((defined(__GNUC__) && (__GNUC__ >= 3)) || \
+     (defined(__clang__) && __has_builtin(__builtin_clz))) && \
+    (!defined(_WIN32))
+#  define HAVE___BUILTIN_CLZ 1
+#endif
