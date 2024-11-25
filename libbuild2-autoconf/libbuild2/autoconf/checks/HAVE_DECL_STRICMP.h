46e14cf5e1626fdac5f450a7d9f416265cc5a1a7
helmesjo <helmesjo@gmail.com>
2024-11-25 12:18:58 +0100

    Add HAVE_DECL_STRICMP.h

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_DECL_STRICMP.h
@@ -0,0 +1,19 @@
+// HAVE_DECL_STRICMP : BUILD2_AUTOCONF_LIBC_VERSION
+
+#ifndef BUILD2_AUTOCONF_LIBC_VERSION
+#  error BUILD2_AUTOCONF_LIBC_VERSION appears to be conditionally included
+#endif
+
+#undef HAVE_DECL_STRICMP
+
+/* Indicates the declaration of stricmp(), which
+ * performs case-insensitive string comparison.
+ * Available on Windows and MinGW.
+ */
+#if defined(__linux__) || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(1, 0) || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(1, 0) || \
+    BUILD2_AUTOCONF_OPENBSD_PREREQ(199610) || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0)
+#  define HAVE_DECL_STRICMP 1
+#endif
