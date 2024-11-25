e0657fd22b6ad86c8b17231b37a6e21ea2566e75
helmesjo <helmesjo@gmail.com>
2024-11-25 12:19:01 +0100

    Add HAVE_FUNC_ATTRIBUTE1_ALLOC_SIZE.h

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_ATTRIBUTE1_ALLOC_SIZE.h
@@ -0,0 +1,12 @@
+// HAVE_FUNC_ATTRIBUTE1_ALLOC_SIZE
+
+#undef HAVE_FUNC_ATTRIBUTE1_ALLOC_SIZE
+
+/* Checks ensure that the compiler supports the alloc_size
+ * function attribute, which specifies that a function
+ * returns a pointer to allocated memory and optionally
+ * indicates the size of the allocation.
+ */
+#if defined(__GNUC__) && (__GNUC__ >= 4)
+#  define HAVE_FUNC_ATTRIBUTE1_ALLOC_SIZE 1
+#endif
