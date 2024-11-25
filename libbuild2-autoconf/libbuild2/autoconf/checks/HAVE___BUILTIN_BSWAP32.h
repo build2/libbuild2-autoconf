a65fd49d41a479c8abd89aabe89ed89517862b3e
helmesjo <helmesjo@gmail.com>
2024-11-25 12:19:10 +0100

    Add HAVE___BUILTIN_BSWAP32.h

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE___BUILTIN_BSWAP32.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE___BUILTIN_BSWAP32.h
@@ -2,9 +2,15 @@
 
 #undef HAVE___BUILTIN_BSWAP32
 
-/* Checks for the GCC/Clang built-in function __builtin_bswap32(),
- * which provides branch prediction hints.
+/* Checks for the availability of the __builtin_bswap32
+ * macro or intrinsic, which swaps the byte order
+ * of a 32-bit integer.
+ * It is typically defined in system headers such
+ * as <byteswap.h> on GNU-based systems or equivalent
+ * headers on other platforms.
  */
-#if defined(__GNUC__) || defined(__clang__)
+#if ((defined(__GNUC__) && (__GNUC__ >= 5)) || \
+     (defined(__clang__) && __has_builtin(__builtin_bswap32))) && \
+    (!defined(_WIN32))
 #  define HAVE___BUILTIN_BSWAP32 1
 #endif
