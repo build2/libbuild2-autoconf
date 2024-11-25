8778488647f528c7d580d8d686e0b46856007037
helmesjo <helmesjo@gmail.com>
2024-11-25 12:19:10 +0100

    Add HAVE___BUILTIN_EXPECT.h

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE___BUILTIN_EXPECT.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE___BUILTIN_EXPECT.h
@@ -5,6 +5,6 @@
 /* Checks for the GCC/Clang built-in function __builtin_expect(),
  * which provides branch prediction hints.
  */
-#if defined(__GNUC__) || defined(__clang__)
+#if defined(__GNUC__) || (defined(__clang__) && __has_builtin(__builtin_expect))
 #  define HAVE___BUILTIN_BSEXPECT
 #endif
