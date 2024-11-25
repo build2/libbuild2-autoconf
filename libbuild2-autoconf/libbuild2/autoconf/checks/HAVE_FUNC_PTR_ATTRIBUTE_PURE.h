23c4a1d361be9d6bd75e4fc970f572a311cae89a
helmesjo <helmesjo@gmail.com>
2024-11-25 12:19:04 +0100

    Add HAVE_FUNC_PTR_ATTRIBUTE_PURE.h

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_PURE.h
@@ -0,0 +1,11 @@
+// HAVE_FUNC_PTR_ATTRIBUTE_PURE
+
+#undef HAVE_FUNC_PTR_ATTRIBUTE_PURE
+
+/* Checks if the compiler supports the pure function attribute,
+ * indicating a function pointer return value depends only on its
+ * arguments, with no global side effects.
+ */
+#if defined(__GNUC__) || defined(__clang__)
+#  define HAVE_FUNC_PTR_ATTRIBUTE_PURE 1
+#endif
