36a300095b65f1f0c1f6712261eb7eb5d181f5ed
helmesjo <helmesjo@gmail.com>
2024-11-25 14:39:46 +0100

    Add HAVE___BUILTIN_BSWAP64.h

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE___BUILTIN_BSWAP64.h
@@ -0,0 +1,16 @@
+// HAVE___BUILTIN_BSWAP64
+
+#undef HAVE___BUILTIN_BSWAP64
+
+/* Checks for the availability of the __builtin_bswap64
+ * macro or intrinsic, which swaps the byte order
+ * of a 64-bit integer.
+ * It is typically defined in system headers such
+ * as <byteswap.h> on GNU-based systems or equivalent
+ * headers on other platforms.
+ */
+#if ((defined(__GNUC__) && (__GNUC__ >= 5)) || \
+     (defined(__clang__) && __has_builtin(__builtin_bswap64))) && \
+    (!defined(_WIN32))
+#  define HAVE___BUILTIN_BSWAP64 1
+#endif
