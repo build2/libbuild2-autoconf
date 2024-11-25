1a93dd52208b7af94bd54a9030dc04e34a6e12b7
helmesjo <helmesjo@gmail.com>
2024-11-25 12:19:02 +0100

    Add HAVE_FUNC_ATTRIBUTE_RETURNS_NONNULL.h

---
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_ATTRIBUTE_RETURNS_NONNULL.h
@@ -0,0 +1,10 @@
+// HAVE_FUNC_ATTRIBUTE_RETURNS_NONNULL
+
+#undef HAVE_FUNC_ATTRIBUTE_RETURNS_NONNULL
+
+/* Checks if the compiler supports the unused attribute for function
+ * pointers, which can suppress warnings for unused function pointers.
+ */
+#if defined(__GNUC__) || defined(__clang__)
+#  define HAVE_FUNC_PTR_ATTRIBUTE_RETURNS_NONNULL 1
+#endif
