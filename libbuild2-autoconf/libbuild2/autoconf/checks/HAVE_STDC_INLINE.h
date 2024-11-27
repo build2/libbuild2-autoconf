e06bdec61cec4f2d6a086b43a0f57bb1f6fd889e
helmesjo <helmesjo@gmail.com>
2024-11-27 22:37:27 +0100

    Updated check HAVE_STDC_INLINE for MSVC/MinGW/Cygwin/GCC/Clang

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STDC_INLINE.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STDC_INLINE.h
@@ -5,6 +5,8 @@
 /* Checks if the compiler supports standard inline functions
  * according to the C99 specification (inline).
  */
-#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
+#if (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)) || \
+    (defined(__clang__) || defined(__GNUC__)) || \
+    (defined(_MSC_VER) && (_MSC_VER >= 1800)) /* MSVC 2013+ */
 #  define HAVE_STDC_INLINE 1
 #endif
