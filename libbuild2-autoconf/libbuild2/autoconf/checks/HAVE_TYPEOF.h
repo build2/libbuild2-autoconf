fd027d00d1fb0b96624edac1cdb3780237f6c1e2
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:46 +0100

    Updated check HAVE_TYPEOF for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_TYPEOF.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_TYPEOF.h
@@ -4,7 +4,10 @@
 
 /* Checks for support of the GNU extension typeof,
  * which allows type inference in C.
+ * Supported by GCC ≥ 2.5, Clang ≥ 3.0, and some other compilers.
  */
-#if defined(__GNUC__) || defined(__clang__)
+
+#if (defined(__GNUC__) && (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 5))) || \
+    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 3))
 #  define HAVE_TYPEOF 1
 #endif
