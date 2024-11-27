f44e84c8d87f956ee9300698e7d8439a790597ac
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:43 +0100

    Updated check HAVE_FUNC_ATTRIBUTE3_FORMAT for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_ATTRIBUTE3_FORMAT.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_ATTRIBUTE3_FORMAT.h
@@ -5,6 +5,7 @@
 /* Checks if the compiler supports the format attribute, allowing
  * type-checking of printf/scanf-style function arguments.
  */
-#if defined(__GNUC__) || defined(__clang__)
+#if (defined(__GNUC__) && (__GNUC__ >= 3)) || \
+    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 3))
 #  define HAVE_FUNC_ATTRIBUTE3_FORMAT 1
 #endif
