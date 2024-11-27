b7ba1e11624aac347b4545968bf33ed206ecc6ef
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:42 +0100

    Updated check HAVE_DECL_STRNLEN for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_DECL_STRNLEN.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_DECL_STRNLEN.h
@@ -10,6 +10,9 @@
 #if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 10) || \
     BUILD2_AUTOCONF_FREEBSD_PREREQ(8, 0) || \
     BUILD2_AUTOCONF_NETBSD_PREREQ(6, 0) || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 7)
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 7) || \
+    defined(_WIN32) || \
+    defined(__MINGW32__) || \
+    defined(__CYGWIN__)
 #  define HAVE_DECL_STRNLEN 1
 #endif
