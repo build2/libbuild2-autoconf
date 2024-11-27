063f195088fff9577bcf230d49961d08ee91e01d
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:43 +0100

    Updated check HAVE_FUNC_ATTRIBUTE_MALLOC for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_ATTRIBUTE_MALLOC.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_ATTRIBUTE_MALLOC.h
@@ -5,6 +5,7 @@
 /* Checks if the compiler supports the malloc function attribute,
  * which indicates that the function does not return to the caller.
  */
-#if defined(__GNUC__) || defined(__clang__)
+#if (defined(__GNUC__) && (__GNUC__ >= 3)) || \
+    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 3))
 #  define HAVE_FUNC_ATTRIBUTE_MALLOC 1
 #endif
