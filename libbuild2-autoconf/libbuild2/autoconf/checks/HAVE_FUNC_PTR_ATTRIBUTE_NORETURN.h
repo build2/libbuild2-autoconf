b2cfd2fdd75a324db1c7996342d29ac55ffc1a5c
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:45 +0100

    Updated check HAVE_FUNC_PTR_ATTRIBUTE_NORETURN for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_NORETURN.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_NORETURN.h
@@ -6,6 +6,7 @@
  * for function pointers, indicating that the function
  * does not return.
  */
-#if defined(__GNUC__) || defined(__clang__)
-#  define HAVE_FUNC_ATPTR_ATTRIBUTE_NORETURN
+#if (defined(__GNUC__) && (__GNUC__ >= 2)) || \
+    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 3))
+#  define HAVE_FUNC_PTR_ATTRIBUTE_NORETURN 1
 #endif
