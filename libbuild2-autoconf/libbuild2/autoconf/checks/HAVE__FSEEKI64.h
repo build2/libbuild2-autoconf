078e146b7176e94b65ee301b69803359ec4cec04
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:46 +0100

    Updated check HAVE__FSEEKI64 for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE__FSEEKI64.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE__FSEEKI64.h
@@ -6,9 +6,12 @@
  * Microsoft-specific function that moves the file
  * position indicator for a file stream using a
  * 64-bit integer offset.
- * It is the Windows equivalent of fseeko() in
- * POSIX environments and supports large files.
+ * - Available on MSVC since Visual Studio 2005 (_MSC_VER >= 1400)
+ * - Available on MinGW (with MSVCRT runtime) when targeting Windows
+ * - Not available on POSIX or other non-Windows environments.
  */
-#if defined(_WIN32) || defined(__MINGW32__)
+
+#if (defined(_WIN32) && defined(_MSC_VER) && (_MSC_VER >= 1400)) || \
+    (defined(__MINGW32__) && !defined(__CYGWIN__))
 #  define HAVE__FSEEKI64 1
 #endif
