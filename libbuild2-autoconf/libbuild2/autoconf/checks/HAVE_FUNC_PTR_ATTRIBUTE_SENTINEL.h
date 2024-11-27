55d6d6c65de0565e49897c11c98209034ae100b4
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:45 +0100

    Updated check HAVE_FUNC_PTR_ATTRIBUTE_SENTINEL for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_SENTINEL.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_SENTINEL.h
@@ -6,6 +6,7 @@
  * attribute, which enforces a null-terminated
  * argument list for variadic function pointers.
  */
-#if defined(__GNUC__) || defined(__clang__)
+#if (defined(__GNUC__) && (__GNUC__ >= 3)) || \
+    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 3))
 #  define HAVE_FUNC_PTR_ATTRIBUTE_SENTINEL 1
 #endif
