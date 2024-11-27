2856e58684a9e9198732a79792527157757669f0
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:45 +0100

    Updated check HAVE_FUNC_PTR_ATTRIBUTE_PURE for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_PURE.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_PURE.h
@@ -6,6 +6,7 @@
  * indicating a function pointer return value depends only on its
  * arguments, with no global side effects.
  */
-#if defined(__GNUC__) || defined(__clang__)
+#if (defined(__GNUC__) && (__GNUC__ >= 3)) || \
+    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 3))
 #  define HAVE_FUNC_PTR_ATTRIBUTE_PURE 1
 #endif
