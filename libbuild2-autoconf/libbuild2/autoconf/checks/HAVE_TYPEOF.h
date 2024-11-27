d6ef26ab62f89dd93b3243901dd74de8bfcc18de
helmesjo <helmesjo@gmail.com>
2024-11-28 00:59:18 +0100

    Updated check HAVE_TYPEOF to always '#define typeof __typeof__' if true.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_TYPEOF.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_TYPEOF.h
@@ -2,15 +2,16 @@
 
 #undef HAVE_TYPEOF
 
-/* Checks for support of the GNU extension typeof,
+/* Checks for support of the GNU extension `typeof`,
  * which allows type inference in C.
- * Supported by GCC ≥ 2.5, Clang ≥ 3.0, and some other compilers.
+ * - Supported by GCC ≥ 2.5.
+ * - Supported by Clang ≥ 3.0 (including AppleClang).
+ * - Not supported by MSVC.
+ * - Supported on most platforms where GCC/Clang is used, except limited environments.
  */
 
 #if (defined(__GNUC__) && (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 5))) || \
-    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 3))
+    (defined(__clang__) && (__clang_major__ >= 3))
 #  define HAVE_TYPEOF 1
-#  ifdef __APPLE__
 #  define typeof __typeof__
-#  endif
 #endif
