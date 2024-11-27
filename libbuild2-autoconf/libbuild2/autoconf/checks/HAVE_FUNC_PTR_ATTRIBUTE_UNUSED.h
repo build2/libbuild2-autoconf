989daf896dfca9f9f4d60ecff28a995fce688c4e
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:45 +0100

    Updated check HAVE_FUNC_PTR_ATTRIBUTE_UNUSED for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_UNUSED.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_UNUSED.h
@@ -5,6 +5,7 @@
 /* Checks if the compiler supports the unused attribute for function
  * pointers, which can suppress warnings for unused function pointers.
  */
-#if defined(__GNUC__) || defined(__clang__)
-#  define HAVE_FUNC_PTR_PTR_ATTRIBUTE_UNUSED 1
+#if (defined(__GNUC__) && (__GNUC__ >= 2)) || \
+    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 3))
+#  define HAVE_FUNC_PTR_ATTRIBUTE_UNUSED 1
 #endif
