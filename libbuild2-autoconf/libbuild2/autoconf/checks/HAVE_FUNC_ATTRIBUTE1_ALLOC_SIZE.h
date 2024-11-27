7ad63c40be45c2d630d65dc0d82f9f70db64f7c2
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:42 +0100

    Updated check HAVE_FUNC_ATTRIBUTE1_ALLOC_SIZE for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_ATTRIBUTE1_ALLOC_SIZE.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_ATTRIBUTE1_ALLOC_SIZE.h
@@ -7,6 +7,7 @@
  * returns a pointer to allocated memory and optionally
  * indicates the size of the allocation.
  */
-#if defined(__GNUC__) && (__GNUC__ >= 4)
+#if (defined(__GNUC__) && (__GNUC__ >= 4)) || \
+    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 3))
 #  define HAVE_FUNC_ATTRIBUTE1_ALLOC_SIZE 1
 #endif
