64d84caec638d41b30f47b94676f411ad311857e
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:42 +0100

    Updated check HAVE_FSEEKO for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FSEEKO.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FSEEKO.h
@@ -9,9 +9,12 @@
 /* This checks for fseeko(), which is a large-file-aware
  * alternative to fseek().
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1) || \
-    BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 0) || \
-    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0) || \
-    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0)
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)    || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(3, 0)  || \
+    BUILD2_AUTOCONF_NETBSD_PREREQ(2, 0)   || \
+    BUILD2_AUTOCONF_MACOS_PREREQ(10, 0)   || \
+    defined(__CYGWIN__)                   || \
+    defined(__MINGW32__)                  || \
+    (defined(__sun) && defined(__SVR4))   // Solaris
 #  define HAVE_FSEEKO 1
 #endif
