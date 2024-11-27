8673a70efdc74d902bb41ae2545cf6d1ac7736f2
helmesjo <helmesjo@gmail.com>
2024-11-27 19:00:45 +0100

    Updated check HAVE_FUNC_PTR_ATTRIBUTE_RETURNS_NONNULL for MSVC/MinGW/Cygwin

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_RETURNS_NONNULL.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_FUNC_PTR_ATTRIBUTE_RETURNS_NONNULL.h
@@ -5,6 +5,7 @@
 /* Checks if the compiler supports the unused attribute for function
  * pointers, which can suppress warnings for unused function pointers.
  */
-#if defined(__GNUC__) || defined(__clang__)
-#  define HAVE_FUNC_PTR_PTR_ATTRIBUTE_RETURNS_NONNULL 1
+#if (defined(__GNUC__) && (__GNUC__ >= 5)) || \
+    (!defined(_MSC_VER) && defined(__clang__) && (__clang_major__ >= 4))
+#  define HAVE_FUNC_PTR_ATTRIBUTE_RETURNS_NONNULL 1
 #endif
