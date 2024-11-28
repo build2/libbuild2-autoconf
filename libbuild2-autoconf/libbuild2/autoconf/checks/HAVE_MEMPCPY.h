57ea2fdbc815ee49e9312e1f1a8b37a157704a7a
helmesjo <helmesjo@gmail.com>
2024-11-28 01:00:39 +0100

    Updated HAVE_MEMCPY for MinGW/Cygwin.

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_MEMPCPY.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_MEMPCPY.h
@@ -9,6 +9,9 @@
 /* This checks for mempcpy() availability, which copies memory and
  * returns a pointer to the end of the copied data. It's a GNU extension.
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 2)
+#if defined(__GNUC__) && \
+    BUILD2_AUTOCONF_GLIBC_PREREQ(2, 2) || \
+    BUILD2_AUTOCONF_MINGW_PREREQ(1, 0) || \
+    defined(__CYGWIN__)
 #  define HAVE_MEMPCPY 1
 #endif
