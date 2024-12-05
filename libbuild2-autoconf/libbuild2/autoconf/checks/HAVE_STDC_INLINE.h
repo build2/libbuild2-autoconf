def896f8b34e2f32eff00aa5e1aef07c1722ef44
helmesjo <helmesjo@gmail.com>
2024-12-05 22:35:44 +0100

    Tweak HAVE_STDC_INLINE.h (only use '__STDC_VERSION__' to check, not platform).

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STDC_INLINE.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_STDC_INLINE.h
@@ -5,8 +5,6 @@
 /* Checks if the compiler supports standard inline functions
  * according to the C99 specification (inline).
  */
-#if (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)) || \
-    (defined(__clang__) || defined(__GNUC__)) || \
-    (defined(_MSC_VER) && (_MSC_VER >= 1800)) /* MSVC 2013+ */
+#if (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L))
 #  define HAVE_STDC_INLINE 1
 #endif
