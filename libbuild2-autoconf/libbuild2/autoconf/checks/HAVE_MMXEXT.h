dad5612ef6c41f5922b69c38b3243b1a3d3ff90a
helmesjo <helmesjo@gmail.com>
2024-12-18 16:23:32 +0100

    Revise HAVE_MMXEXT: The '__MMXEXT__' define is barely set by any compiler. Resort to a looser check (x86/x86_64).

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_MMXEXT.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_MMXEXT.h
@@ -5,9 +5,10 @@
 /* Presence of MMXEXT instructions, which are extensions to the original
  * MMX instruction set, available on AMD processors starting from K6-2.
  * Since glibc 2.0 (x86), FreeBSD (x86), OpenBSD (x86), NetBSD (x86),
- * MacOS 10.04 (x86), MSVC
+ * macOS 10.4 (x86), MSVC
  */
 #if defined(__MMXEXT__) || \
-    defined(_M_IX86) || defined(_M_X64)
+    (defined(__i386__) || defined(__x86_64__)) || \
+    (defined(_M_IX86) || defined(_M_X64))
 #  define HAVE_MMXEXT 1
 #endif
