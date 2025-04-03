128edfba6855b9a122fca659e8aac780adbd403c
helmesjo <helmesjo@gmail.com>
2025-04-03 12:29:02 +0200

    Revise HAVE_ALTIVEC (cover more cases explicitly).

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ALTIVEC.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_ALTIVEC.h
@@ -2,8 +2,14 @@
 
 #undef HAVE_ALTIVEC
 
-/* PowerPC (POWER1 and later) + compiler flag
+/* PowerPC (POWER1 and later)
  */
-#if defined(__VEC__) || defined(__ALTIVEC__)
+#if defined(__powerpc) || \
+    defined(__powerpc__) || \
+    defined(__powerpc64__) || \
+    defined(__POWERPC__) || \
+    defined(__ppc__) || \
+    defined(__PPC__) || \
+    defined(_ARCH_PPC)
 #  define HAVE_ALTIVEC 1
 #endif
