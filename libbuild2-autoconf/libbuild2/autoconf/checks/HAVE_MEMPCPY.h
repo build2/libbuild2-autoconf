1e303f3bd0222ebe94497a61e7737dbf1223425a
Francois Kritzinger <francoisk777@gmail.com>
2025-06-02 14:10:05 +0200

    Fix HAVE_MEMPCPY (add FreeBSD)

--- libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_MEMPCPY.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/HAVE_MEMPCPY.h
@@ -10,9 +10,13 @@
  * to the end of the copied data.
  *
  * This is a GNU extension. Not available on Windows (except Mingw-w64).
+ *
+ * @@ TODO mempcpy() is present in NetBSD-current but not yet released so keep
+ *    an eye on that.
  */
-#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1) || \
-    BUILD2_AUTOCONF_MINGW_PREREQ(2, 0) || \
+#if BUILD2_AUTOCONF_GLIBC_PREREQ(2, 1)    || \
+    BUILD2_AUTOCONF_FREEBSD_PREREQ(13, 1) || \
+    BUILD2_AUTOCONF_MINGW_PREREQ(2, 0)    || \
     defined(__CYGWIN__)
 #  define HAVE_MEMPCPY 1
 #endif
