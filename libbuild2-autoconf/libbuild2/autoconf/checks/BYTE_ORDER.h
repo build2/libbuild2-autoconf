40a5027734039ba00f545e4e2f48a60aa02b758f
Karen Arutyunov <karen@codesynthesis.com>
2026-02-20 21:22:16 +0200

    Fix BYTE_ORDER check
    Prefer GCC and Clang predefined macros __BYTE_ORDER__, __ORDER_BIG_ENDIAN__,
    and __ORDER_LITTLE_ENDIAN__, if available.

--- libbuild2-autoconf/libbuild2/autoconf/checks/BYTE_ORDER.h
+++ libbuild2-autoconf/libbuild2/autoconf/checks/BYTE_ORDER.h
@@ -1,56 +1,75 @@
 // BYTE_ORDER!
 
-/* Include the endianness header based on platform.
+/* Define BYTE_ORDER, BIG_ENDIAN, and LITTLE_ENDIAN macros.
  *
- * Each of these headers should define BYTE_ORDER, LITTLE_ENDIAN, BIG_ENDIAN,
- * AND PDP_ENDIAN but this can be affected by macros like _ANSI_SOURCE,
- * _POSIX_C_SOURCE, _XOPEN_SOURCE and _NETBSD_SOURCE, depending on the
- * platform (in which case most of them define underscored versions only).
- */
-#if defined(__GLIBC__) || defined(__OpenBSD__)
-#  include <endian.h>
-#elif defined(__FreeBSD__) || defined(__NetBSD__)
-#  include <sys/endian.h>
-#elif defined(__APPLE__)
-#  include <machine/endian.h>
-#elif !defined(_WIN32)
-#  include <sys/param.h>
-#endif
-
-/* Try various system- and compiler-specific byte order macro names if the
- * endianness headers did not define BYTE_ORDER.
+ * Note that GCC (since 4.6) and Clang (since 3.1) provide predefined macros
+ * __BYTE_ORDER__, __ORDER_BIG_ENDIAN__, and __ORDER_LITTLE_ENDIAN__. Prefer
+ * these macros, if available. Otherwise, use the platform-specific headers
+ * for POSIX systems and provide custom macro definitions on Windows.
  */
 #if !defined(BYTE_ORDER)
-#  if defined(__linux__)
-#    if defined(__BYTE_ORDER)
-#      define BYTE_ORDER    __BYTE_ORDER
-#      define BIG_ENDIAN    __BIG_ENDIAN
-#      define LITTLE_ENDIAN __LITTLE_ENDIAN
-#    endif
-#  elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
-#    if defined(_BYTE_ORDER)
-#      define BYTE_ORDER    _BYTE_ORDER
-#      define BIG_ENDIAN    _BIG_ENDIAN
-#      define LITTLE_ENDIAN _LITTLE_ENDIAN
-#    endif
-#  elif defined(__APPLE__)
-#    if defined(__DARWIN_BYTE_ORDER)
-#      define BYTE_ORDER    __DARWIN_BYTE_ORDER
-#      define BIG_ENDIAN    __DARWIN_BIG_ENDIAN
-#      define LITTLE_ENDIAN __DARWIN_LITTLE_ENDIAN
-#    endif
-#  elif defined(_WIN32)
-#    define BIG_ENDIAN    4321
-#    define LITTLE_ENDIAN 1234
-#    define BYTE_ORDER    LITTLE_ENDIAN
-#  elif defined(__BYTE_ORDER__) &&       \
-        defined(__ORDER_BIG_ENDIAN__) && \
-        defined(__ORDER_LITTLE_ENDIAN__)
-     /* GCC, Clang (and others, potentially).
-      */
+#  if defined(__BYTE_ORDER__)       && \
+      defined(__ORDER_BIG_ENDIAN__) && \
+      defined(__ORDER_LITTLE_ENDIAN__)
 #    define BYTE_ORDER    __BYTE_ORDER__
 #    define BIG_ENDIAN    __ORDER_BIG_ENDIAN__
 #    define LITTLE_ENDIAN __ORDER_LITTLE_ENDIAN__
+#  else
+    /* Note that these headers normally define BYTE_ORDER, LITTLE_ENDIAN, and
+     * BIG_ENDIAN. However, they can be affected by feature macros like
+     * _ANSI_SOURCE, _POSIX_C_SOURCE, _XOPEN_SOURCE, or _NETBSD_SOURCE,
+     * depending on the platform. In such cases they may define the
+     * underscored versions only.
+     */
+#    if defined(__GLIBC__) || defined(__OpenBSD__)
+      /* POSIX header. Available on Linux, FreeBSD, but not MacOS.
+       */
+#      include <endian.h>
+#    elif defined(__FreeBSD__) || defined(__NetBSD__)
+       /* Not a POSIX header. Normally available on BSD-derived systems, in
+        * particular, on FreeBSD but not MacOS.
+        */
+#      include <sys/endian.h>
+#    elif defined(__APPLE__)
+       /* Not a POSIX header. Normally available on BSD-derived systems, in
+        * particular, on FreeBSD and MacOS.
+        */
+#      include <machine/endian.h>
+#    elif !defined(_WIN32)
+       /* Not a POSIX header. Normally available on POSIX systems, in
+        * particular, on Linux, FreeBSD, and MacOS.
+        */
+#      include <sys/param.h>
+#    endif
+
+    /* If the endianness headers did not define BYTE_ORDER, then try various
+     * system-specific byte order macro names.
+     */
+#    if !defined(BYTE_ORDER)
+#      if defined(__linux__)
+#        if defined(__BYTE_ORDER)
+#          define BYTE_ORDER    __BYTE_ORDER
+#          define BIG_ENDIAN    __BIG_ENDIAN
+#          define LITTLE_ENDIAN __LITTLE_ENDIAN
+#        endif
+#      elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#        if defined(_BYTE_ORDER)
+#          define BYTE_ORDER    _BYTE_ORDER
+#          define BIG_ENDIAN    _BIG_ENDIAN
+#          define LITTLE_ENDIAN _LITTLE_ENDIAN
+#        endif
+#      elif defined(__APPLE__)
+#        if defined(__DARWIN_BYTE_ORDER)
+#          define BYTE_ORDER    __DARWIN_BYTE_ORDER
+#          define BIG_ENDIAN    __DARWIN_BIG_ENDIAN
+#          define LITTLE_ENDIAN __DARWIN_LITTLE_ENDIAN
+#        endif
+#      elif defined(_WIN32)
+#        define BIG_ENDIAN    4321
+#        define LITTLE_ENDIAN 1234
+#        define BYTE_ORDER    LITTLE_ENDIAN
+#      endif
+#    endif
 #  endif
 #endif
 
